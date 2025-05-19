f8fd685d7f9517dc6f54328bc933b2e2514d0f23
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2025-05-19 10:55:42 +0300

    0.0001 Version.dev.Build

---
+++ warehousing/warehousing/doctype/warehouse_settings/warehouse_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Amos Kiprotich Rono and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Warehouse Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
